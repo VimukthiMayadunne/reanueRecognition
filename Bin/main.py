12d64ecbd6c75b42f9d89fe709ffc1ed4f64fa10
VimukthiMayadunne <vimukthimayadunne@gmail.com>
2020-04-26 17:28:16 +0530

    Calcutating PropotionOfTotalStandAlonePrice

--- Bin/main.py
+++ Bin/main.py
@@ -3,7 +3,9 @@ from Bin.Support.getInput import setContractData, setPerformanceObligations
 newContract = setContractData()
 performanceObligations = setPerformanceObligations(newContract)
 
-totStandalonePrice = sum(c.standAlonePriceForPerformanceObligation for c in performanceObligations)
+totStandAlonePrice = sum(c.standAlonePriceForPerformanceObligation for c in performanceObligations)
 transactionPrice = newContract.getTransactionPrice()
 print(transactionPrice)
-print(totStandalonePrice)
+print(totStandAlonePrice)
+for i in range(len(performanceObligations)):
+    print(performanceObligations[i].getPropotionOfTotalStandAlonePrice(totStandAlonePrice))
