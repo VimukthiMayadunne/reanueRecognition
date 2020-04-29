a168ad4bb78c832d755ef99493da5554b2b6b152
VimukthiMayadunne <vimukthimayadunne@gmail.com>
2020-04-29 17:55:45 +0530

    Minnor adjusments

--- Bin/Server/Server.py
+++ Bin/Server/Server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from Bin.Support.sendCsv import sendCsv
+
 from Bin.Server.serverInput import appendData
 
 app = Flask(__name__)
@@ -17,7 +19,7 @@ def treatment():
     if request.method == 'POST':
         data = request.json
         results = appendData(data)
-        return jsonify(isError=False, message="Success", statusCode=200, data=results), 200
+        return sendCsv(results, 'results.csv', ['Name', 'Time', 'DrCr', 'AccountName', 'Amount'])
 
 
 if __name__ == '__main__':
